0702510f9c5eb43fa38a4469af77829cb8ca727c
唐向龙 <tangxianglong@macair.local>
2016-03-30 08:46:12 +0800

    Initial Commit

---
+++ Leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Leetcode
+//
+//  Created by 唐向龙 on 16/3/30.
+//  Copyright © 2016年 唐向龙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
